723eef559c0b081ca2632a537f1fbfa8aac64a3c
familagash <familagash@gmail.com>
2019-10-09 00:52:47 +0400

    Updates

---
+++ precache-manifest.d2889780c2a54c376493cb719aff2437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3a1879077d4e9073e67cb02836f7d7",
+    "url": "/Indecision-App/index.html"
+  },
+  {
+    "revision": "d918e9a93695526725ae",
+    "url": "/Indecision-App/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b4c52e1ca5e40bf5c430",
+    "url": "/Indecision-App/static/css/main.824d91f8.chunk.css"
+  },
+  {
+    "revision": "d918e9a93695526725ae",
+    "url": "/Indecision-App/static/js/2.916981de.chunk.js"
+  },
+  {
+    "revision": "b4c52e1ca5e40bf5c430",
+    "url": "/Indecision-App/static/js/main.0062f1e6.chunk.js"
+  },
+  {
+    "revision": "8f5fbd1faa4bee3f40b8",
+    "url": "/Indecision-App/static/js/runtime~main.ea7cc2a9.js"
+  }
+]);
